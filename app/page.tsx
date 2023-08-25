143c121adfabea099a430b7048ddfe61e9cc7752
salatsynskahv <salatsynskahv@gmail.com>
2023-08-25 11:06:17 +0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,212 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main>
+            <section className="hero-section">
+                <header>
+                    <span>
+                        <a href="#projects-section">Projects </a>
+                    </span>
+                    <span>
+                        <a href="#resume-section">Resume </a>
+                    </span>
+                </header>
+                <div className="portfolio-text">
+                    Portfolio
+                </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <div>
+                    <svg className="waves" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                         viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                        <defs>
+                            <path id="gentle-wave"
+                                  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
+                        </defs>
+                        <g className="parallax">
+                            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(240,240,240,0.7)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(240,240,240,0.5)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(240,240,240,0.3)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(240,240,240,0.9)"/>
+                        </g>
+                    </svg>
+                </div>
+            </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <section id="resume-section" className="about-me-section">
+                <div className="container">
+                    {/*<div className="photo"></div>*/}
+                    <div className="person">
+                        <div className="person_container">
+                            <img className="person_circle" src="/img/circle.png" alt=""/>
+                            <img className="person_img" src="/img/ganna-photo.png" alt="Ganna photo"/>
+                        </div>
+                    </div>
+                    <div className="info">
+                        Hello! My name is Ganna Salatsynska.<br/>
+                        I worked as Java Engineer and switched to Frontend. I am a collaborative and team-oriented
+                        person with a proven track record of working effectively in cross-functional teams.
+                        <br/>
+                        Throughout my career, I have been a dedicated team member, always ready to contribute and
+                        collaborate to solve problems.
+                        <br/>
+                        Outside of work, I enjoy engaging in sports and outdoor activities. My hobbies help me maintain
+                        a healthy work-life balance and stay energized and focused.
+                    </div>
+                </div>
+                <br/>
+                <div className="cv-info">
+                    <div className="column">
+                        <h3>Contact</h3>
+                        <p>Phone: +380980183883</p>
+                        <p>Email: salatsynskahv@gmail.com</p>
+                        <br/>
+                        <h3>Skills</h3>
+                        <p>Programming languages and technologies:
+                            JavaScript (React, Angular) - 1.5 year experience.
+                            Java (Java core, Java EE, Spring Framework) - 5 years experience.</p>
+                        <p>Web technologies: HTML, CSS (SASS).</p>
+                        <p>Testing Frameworks: Cypress</p>
+                        <p>CMS: Sanity Studio, Liferay</p>
+                        <p>Build systems: Maven, Ant (+Ivy).</p>
+                        <p>Tools: JntelliJ IDEA, Tortoise SVN, SQL Developer, Postman.</p>
+                    </div>
+                    <div className="column">
+                        <h3>Education</h3>
+                        <p>
+                            <b>National University of 'Kyiv Mohyla Academy' </b>
+                            <br/>
+                            Bachelor of Science: Applied Mathematics
+                            2009 / 2013
+                            <br/><br/>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                            <b>Netcracker Training Center</b> 2012
+                            <br/><br/>
+                            <b>National University of 'Kyiv Mohyla Academy' </b><br/>
+                            Master of Science: Systems and Methods of Decision Making
+                            2013 / 2015
+                        </p>
+                        <br/>
+                        <h3>Work experience</h3>
+                        <p>
+                            Netcracker 2014 / present
+                            Implemented new features of an existing system, contributing to its enhanced functionality
+                            and usability.
+                            Estimated production times and staging requirements for new designs, enabling the team to
+                            deliver projects on time and within budget.
+                            Troubleshot new and existing product problems involving designs, materials, or processes,
+                            ensuring smooth and efficient operation of the system.
+                            Wrote tech design and detailed design specifications, providing an overview of the
+                            architecture, system functions, API, and extension points for product customization.
+                        </p>
+                        <br/>
+                        <h3>Languages</h3>
+                        <ul>
+                            <li>Ukrainian - Native</li>
+                            <li>English - Intermediate</li>
+                            <li>Polish - Basic</li>
+                        </ul>
+                    </div>
+                </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+            </section>
+            <section id="projects-section" className="project-section">
+                <h2>Projects</h2>
+                <div className="desc-container">
+                    <div className="desc-info">
+                        Wishlist - app that allows you to save all your plans for shopping and share it with friends.
+                        App allows you to create custom witlists (tables with list of wishes) and share it with friends.
+                        Technologies used:
+                        <ul>
+                            <li>React</li>
+                            <li>MonogoDB</li>
+                            <li>NodeJs, Express, Mongoose</li>
+                            <li>Firebase</li>
+                        </ul>
+                        <br/>
+                        <a href="https://wishlist-client.onrender.com/" target="_blank"> Live Demo </a>
+                    </div>
+                    <div className="desc-link">
+                        <a href="https://wishlist-client.onrender.com/" target="_blank">
+                            <img src="/img/wishlist-app.png" alt="wishlist screen" />
+                        </a>
+                    </div>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                <div className="desc-container">
+
+                    <div className="desc-info">
+                        Food Delivery service:
+                        <ul>
+                            <li>React</li>
+                            <li>MonogoDB</li>
+                            <li>NodeJs, Express, Mongoose</li>
+                            <li>Firebase</li>
+                        </ul>
+                        <br/>
+                        <a href="https://wishlist-client.onrender.com/" target="_blank"> Live Demo </a>
+                    </div>
+                    <div className="desc-link">
+                        <a href="https://food-delivery-3ft8.onrender.com/" target="_blank">
+                            <img src="/img/food-delivery-app.png" alt="food delivery app"/>
+                        </a>
+                    </div>
+                </div>
+                <div className="desc-container">
+                    <div className="desc-info">
+                        Expense tracker
+                        <ul>
+                            <li>React</li>
+                            <li>MonogoDB</li>
+                            <li>NodeJs, Express, Mongoose</li>
+                            <li>Firebase</li>
+                        </ul>
+                        <br/>
+                        <a href="https://wishlist-client.onrender.com/" target="_blank"> Live Demo </a>
+                    </div>
+                    <div className="desc-link">
+
+                    </div>
+                </div>
+                <div className="desc-container">
+                    <div className="desc-info">
+                        Online shop
+                        <ul>
+                            <li>React</li>
+                            <li>MonogoDB</li>
+                            <li>NodeJs, Express, Mongoose</li>
+                            <li>Firebase</li>
+                        </ul>
+                        <br/>
+                        <a href="https://wishlist-client.onrender.com/" target="_blank"> Live Demo </a>
+                    </div>
+                    <div className="desc-link">
+
+                    </div>
+                </div>
+                <div className="rotate-180">
+                    <svg className="waves-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
+                         viewBox="0 24 150 28" preserveAspectRatio="none" shapeRendering="auto">
+                        <defs>
+                            <path id="gentle-wave"
+                                  d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"/>
+                        </defs>
+                        <g className="parallax">
+                            <use xlinkHref="#gentle-wave" x="48" y="0" fill="rgba(240,240,240,0.7)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="3" fill="rgba(240,240,240,0.5)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="5" fill="rgba(240,240,240,0.3)"/>
+                            <use xlinkHref="#gentle-wave" x="48" y="7" fill="rgba(240,240,240,0.9)"/>
+                        </g>
+                    </svg>
+                </div>
+
+
+            </section>
+
+            <footer>
+
+            </footer>
+
+        </main>
+    )
 }
