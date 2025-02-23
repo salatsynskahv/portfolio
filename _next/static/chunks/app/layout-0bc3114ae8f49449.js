8b945375f2ac70137e232e36093fe5dbbadf6d25
salatsynskahv <salatsynskahv@gmail.com>
2025-02-23 08:12:55 +0200

    Updates

---
+++ _next/static/chunks/app/layout-0bc3114ae8f49449.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{7817:function(n,e,u){Promise.resolve().then(u.t.bind(u,8060,23))},8060:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=7817)}),_N_E=n.O()}]);
